3707b23d242e16c939d56f7280594e03b86b9d47
Valiantsin Palkhouski <thisis.evesko@mail.ru>
2026-09-09 23:23:21 +0300

    fix: add DesignTimeDbContextFactory to sample

---
+++ samples/InsuranceSample/DesignTimeDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace InsuranceSample;
+
+/// <summary>
+/// Used by <c>dotnet ef</c> at design time. The connection string is never opened for
+/// <c>migrations add</c>; it only needs to be syntactically valid for Npgsql.
+/// </summary>
+public sealed class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<InsuranceDbContext>
+{
+    public InsuranceDbContext CreateDbContext(string[] args)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("HINDSIGHT_SAMPLE_CS")
+                               ?? "Host=localhost;Database=hindsight_sample;Username=postgres;Password=postgres";
+
+        var options = new DbContextOptionsBuilder<InsuranceDbContext>()
+            .UseNpgsql(connectionString)
+            .Options;
+
+        return new InsuranceDbContext(options);
+    }
+}
